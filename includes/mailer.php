89ebe7b15c7ad811cb6674b2f20ecdd2ba026752
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2022-02-12 15:01:54 +0300

    new version

---
+++ includes/mailer.php
@@ -0,0 +1,30 @@
+<?php
+require('../vendor/autoload.php');
+
+function mailling($subject, $body){
+$hostname = 'smtp.cloudmta.net';
+$username = '7c3c506dea731b2f';
+$password = 'Mh812rKx4TQCm6XNTjmpGkGB';
+
+$transport = (new Swift_SmtpTransport($hostname, 2525, 'tls'))
+  ->setUsername($username)
+  ->setPassword($password);
+
+$mailer = new Swift_Mailer($transport);
+
+
+$message = (new Swift_Message())
+  ->setSubject($subject)
+  ->setFrom(['info@writtela.com' => 'New Form Submission'])
+  ->setTo(['mcanthony98@gmail.com']);
+
+$headers = ($message->getHeaders())
+  -> addTextHeader('X-CloudMTA-Class', 'standard');
+
+$message->setBody(
+  ''
+);
+$message->addPart($body, 'text/html');
+$mailer->send($message);
+}
+?>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<header class="u-clearfix u-gradient u-header u-sticky u-header" id="sec-cd51"><div class="u-clearfix u-sheet u-sheet-1">
+ <header class="u-clearfix u-gradient u-header u-sticky u-header" id="sec-cd51"><div class="u-clearfix u-sheet u-sheet-1">
         <a href="index.php" data-page-id="431927156" class="u-align-left-xs u-image u-logo u-image-1" data-image-width="352" data-image-height="270" title="Home">
           <img src="images/logo.png" class="u-logo-image u-logo-image-1">
         </a>
@@ -14,12 +14,7 @@
           <div class="u-custom-menu u-nav-container">
             <ul class="u-nav u-spacing-30 u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-border-3 u-border-active-white u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="index.php" style="padding: 10px 0px;">Home</a>
 </li><li class="u-nav-item"><a class="u-border-3 u-border-active-white u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="about.php" style="padding: 10px 0px;">About</a>
-</li><li class="u-nav-item"><a class="u-border-3 u-border-active-white u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="destinations.php" style="padding: 10px 0px;">Destinations</a><div class="u-nav-popup"><ul class="u-h-spacing-20 u-nav u-unstyled u-v-spacing-10 u-nav-2"><li class="u-nav-item"><a class="u-active-palette-3-light-1 u-button-style u-hover-palette-3-light-1 u-nav-link u-white" href="amboseli-national-park.php">Amboseli National Park</a>
-</li><li class="u-nav-item"><a class="u-active-palette-3-light-1 u-button-style u-hover-palette-3-light-1 u-nav-link u-white">Maasai Mara National Park</a>
-</li><li class="u-nav-item"><a class="u-active-palette-3-light-1 u-button-style u-hover-palette-3-light-1 u-nav-link u-white">Nakuru Safari</a>
-</li><li class="u-nav-item"><a class="u-active-palette-3-light-1 u-button-style u-hover-palette-3-light-1 u-nav-link u-white">Samburu Safari</a>
-</li></ul>
-</div>
+</li><li class="u-nav-item"><a class="u-border-3 u-border-active-white u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="destinations.php" style="padding: 10px 0px;">Destinations</a>
 </li><li class="u-nav-item"><a class="u-border-3 u-border-active-white u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="book-flight.php" style="padding: 10px 0px;">Book a flight</a>
 </li><li class="u-nav-item"><a class="u-border-3 u-border-active-white u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="gallery.php" style="padding: 10px 0px;">Gallery</a>
 </li><li class="u-nav-item"><a class="u-border-3 u-border-active-white u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="contact.php" style="padding: 10px 0px;">Contact</a>
@@ -29,14 +24,9 @@
             <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
               <div class="u-inner-container-layout u-sidenav-overflow">
                 <div class="u-menu-close"></div>
-                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-3"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.php" style="padding: 10px 0px;">Home</a>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.php" style="padding: 10px 0px;">Home</a>
 </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="about.php" style="padding: 10px 0px;">About</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="destinations.php" style="padding: 10px 0px;">Destinations</a><div class="u-nav-popup"><ul class="u-h-spacing-20 u-nav u-unstyled u-v-spacing-10 u-nav-4"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="amboseli-national-park.php">Amboseli National Park</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link">Maasai Mara National Park</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link">Nakuru Safari</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link">Samburu Safari</a>
-</li></ul>
-</div>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="destinations.php" style="padding: 10px 0px;">Destinations</a>
 </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="book-flight.php" style="padding: 10px 0px;">Book a flight</a>
 </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="gallery.php" style="padding: 10px 0px;">Gallery</a>
 </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="contact.php" style="padding: 10px 0px;">Contact</a>
