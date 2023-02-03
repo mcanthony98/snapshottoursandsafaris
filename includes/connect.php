eb6c0290ff16de62f0a4a17672afe6648ba80992
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2023-02-03 09:32:34 +0300

    connect db

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'snapshot');
+define('DB_USERNAME', 'u640333703_snapshot');
+define('DB_PASSWORD', '36987412Snapshot');
+define('DB_DATABASE', 'u640333703_snapshot');
 
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
