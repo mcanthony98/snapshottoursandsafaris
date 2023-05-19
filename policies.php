4f099eed5a0b354141afa86c446faa8022313280
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2023-05-19 18:33:05 +0300

    Create policies.php

---
+++ policies.php
@@ -0,0 +1,152 @@
+<?php
+$pg = 6;
+require "includes/connect.php";
+include "includes/functions.php";
+$destqry = "SELECT * FROM destination ORDER BY position ASC";
+$destres = $conn->query($destqry);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Privacy & Terms - Snapshot Tours and Safaris Kenya, Africa</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPT4TS2');</script>
+<!-- End Google Tag Manager -->
+
+<?php include "includes/stylesheets.php";?>
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '175143165180633');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=175143165180633&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
+</head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RM45DWF1DC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RM45DWF1DC');
+</script>
+<body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPT4TS2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<?php include "includes/navbar.php";?>
+
+
+
+<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/banner2.jpg');" data-stellar-background-ratio="0.5">
+<div class="overlay"></div>
+<div class="container">
+<div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
+<div class="col-md-9 ftco-animate pb-5 mb-5 text-center">
+<h1 class="mb-3 bread">Privacy Policy</h1>
+<p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Privacy Policy <i class="ion-ios-arrow-forward"></i></span></p>
+</div>
+</div>
+</div>
+</section>
+
+<section class="ftco-section">
+<div class="container">
+<div class="row justify-content-center pb-5">
+<div class="col-md-12 heading-section text-center ftco-animate">
+<h2 class="mb-4">Privacy Policy</h2>
+<p></p>
+</div>
+</div>
+<div class="row">
+<div class="col-12 ftco-animate">
+
+<div> Last Updated: 31<sup>st</sup> December 2022</div>
+
+<div class="mx-3 pt-3 text-black">
+    <h5>Privacy Policy for Snapshot Tours and Safaris</h5>
+
+    <p>At Snapshot Tours and Safaris, we are committed to protecting the privacy and confidentiality of our customers' personal information. This Privacy Policy outlines how we collect, use, and safeguard the data you provide to us. By engaging with our services, you consent to the practices described in this policy.</p>
+    
+    <h5>Information Collection</h5>
+    
+    <p>We may collect personal information such as your name, email address, phone number, and travel preferences when you fill out our lead generation form or interact with us.
+    We may also collect non-personal information, such as demographic data or website usage statistics, to enhance our services and improve user experiences.</p>
+
+    <h5>Use of Information</h5>
+    
+    <ul>
+        <li>We use the collected information to provide you with personalized travel recommendations, communicate with you regarding your inquiries or bookings, and deliver outstanding customer service.</li>
+
+    <li>Your information helps us tailor our offerings to your preferences, improve our products and services, and provide you with a seamless travel experience.</li>
+    <li>We may use your email address to send you promotional offers, updates, and newsletters, which you can opt-out of at any time.</li>
+    </ul>
+    
+    <h5>Information Sharing</h5>
+    
+    <p>We do not sell, rent, or trade your personal information to third parties without your consent, except as required by law or as necessary to fulfill your travel arrangements.</p>
+    <p>We may share your information with trusted partners, such as hotels, airlines, and tour operators, to facilitate your bookings and ensure the smooth execution of your travel plans.</p>
+
+    <h5>Data Security</h5>
+    
+    <p>We employ industry-standard security measures to protect your personal information from unauthorized access, alteration, or disclosure. However, please note that no method of data transmission over the internet or electronic storage is 100% secure. While we strive to protect your information, we cannot guarantee absolute security.</p>
+    
+    <h5>Data Retention</h5>
+    
+    <p>We retain your personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy or as required by law. If you wish to request the deletion of your personal data or update your preferences, please contact us using the information provided at the end of this policy.</p>
+
+    <h5>Third-Party Links</h5>
+    
+    <p>Our website or communications may contain links to third-party websites or services. We are not responsible for the privacy practices or content of these external sites. We encourage you to review their respective privacy policies.</p>
+        
+    <h5>Updates to the Privacy Policy</h5>
+    
+    <p>We reserve the right to update or modify this Privacy Policy at any time. Any changes will be effective immediately upon posting the revised policy on our website. We encourage you to review this page periodically to stay informed about our privacy practices.
+    If you have any questions or concerns regarding our privacy practices or wish to exercise your rights regarding your personal information, please contact us at:</p>
+    
+    Snapshot Tours and Safaris<br>
+    Address: Office 4, Hotel Lillies, Juja, Nairobi, Kenya. <br>
+    Email: info@snapshottoursandsafaris.com <br>
+    Phone: +254 7599 49546 <br>
+    
+    
+</div>
+</div>
+
+</div>
+</div>
+</section>
+
+<?php 
+include "includes/footer.php";
+include "includes/scripts.php";
+?>
+
+
+</body>
+
+</html>
