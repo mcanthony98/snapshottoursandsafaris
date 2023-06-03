9ac9ba91b8a0ae6c7234591115bad48de336f9e5
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2023-06-03 13:18:32 +0300

    conversion tracking on Thankyou

--- thank-you-subscribe.php
+++ thank-you-subscribe.php
@@ -21,7 +21,7 @@ if(isset($_SESSION['thank_you_name'])){
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MPT4TS2');</script>
+})(window,document,'script','dataLayer','GTM-MPT4TS2');gtag('config', 'AW-11135189400');</script>
 <!-- End Google Tag Manager -->
 
 <?php include "includes/stylesheets.php";?>
